768452fd954448c3545fc0d9e299d459cb7b4010
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-07-10 21:58:15 +0000

    Create rotate_2d_vector.cpp

---
+++ rotate_2d_vector.cpp
@@ -0,0 +1,32 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    vector<vector<int> > myvector={{1,2,3}, {4,5,6}, {7,8,9}};
+    // step-1: reverse the vector
+    reverse(myvector.begin(), myvector.end());
+       
+  // step-2: flip elements along diagonal
+	int size = myvector.size();
+	for (int i = 0; i < size; ++i)
+    {
+		for (int j = 0; j < i; ++j)
+		{
+			swap(myvector[i][j], myvector[j][i]);
+		}
+	}
+  
+  //print the rotated 2D vector
+    for(int i=0; i<myvector.size(); i++)
+    {
+        for(int j=0; j<myvector[0].size(); j++) 
+        {
+            cout << myvector[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
