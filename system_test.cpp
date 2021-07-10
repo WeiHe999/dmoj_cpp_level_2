7caac8eae9890c2a9729fbf3442c4b82354c548a
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-07-10 07:56:08 -0400

    Update system_test.cpp

--- system_test.cpp
+++ system_test.cpp
@@ -1,62 +1,36 @@
-#include <iostream>
-#include <string>
 #include <vector>
 #include <algorithm>
-#include <sstream>
 #include <stdio.h>
 
 using namespace std;
-template < typename T > void
-print (T t)
-{
- for (const auto & e:t) cout << e << " ";
-  cout << endl;
-}
 
-vector<int>
-split(string str1)
-{
-    vector<int> vv;
-    string word;
-    istringstream ss(str1);
-    while (getline(ss, word, ' ')) {
-        vv.push_back(stoi(word));
-    }
-    return vv;
-}
 
 int main()
 {
-    int n;
-    cin >> n;
-    cin.ignore();
-    vector<vector<int> > vec1;
-    vector<string> v_s;
+    int n1, n2;
+    scanf("%d", &n1);
+    int arr1[n1], arr2[n1], arr3[n1];
     int s = 0;
-    for (int x = 0; x < n; x++) {
-        string line;
-        getline(cin, line);
-        v_s.push_back(line);
+    for (int k = 0; k < n1; k++) {
+        scanf("%d %d %d", &arr1[k], &arr2[k], &arr3[k]);
     }
 
-    cin >> n;
-    int arr[n];
-    for(int i = 0; i < n; i++) scanf("%d", &arr[i]);
-    vector<int> vect_q(arr, arr+n);
+    scanf("%d", &n2);
+    int arr[n2];
+    for(int i = 0; i < n2; i++) scanf("%d", &arr[i]);
+    vector<int> vect_q(arr, arr+n2);
     sort(vect_q.begin(), vect_q.end());
     
-    for (int m=0; m<v_s.size(); m++)
+    for (int m=0; m<n1; m++)
     {
-        
-        vector<int> vec3 = split(v_s[m]);
-        if (upper_bound (vect_q.begin(), vect_q.end(), vec3[1]) - 
-        lower_bound (vect_q.begin(), vect_q.end(), vec3[0]) ==0)
+        if (upper_bound (vect_q.begin(), vect_q.end(), arr2[m]) - 
+        lower_bound (vect_q.begin(), vect_q.end(), arr1[m]) ==0)
         {
-            s += vec3[2];
+            s += arr3[m];
         }
         
     }
- 
-    cout << s;
+
+    printf("%d", s);
     return 0;
 }
