6e92308a3b6cc2fd175b74c504cdc048c8b6c333
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-08-18 18:12:22 -0400

    Create balance.cpp

---
+++ level-2/08-21-2021/balance.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+    cin.sync_with_stdio (0); cin.tie (0);
+    string line;
+    getline(cin, line);
+    stack <char> s1;
+    s1.push(line[0]);
+    for (int a = 1; a < line.length(); a++)
+    {
+        if (!s1.empty() && line[a] == ')' && s1.top() == '(') s1.pop();
+        else s1.push(line[a]);
+    }
+    if (s1.size() == 0 || s1.size() == 2) cout << "YES" << endl;
+    else cout << "NO" << endl;
+}
