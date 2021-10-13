6d4b2d50a35a8d4be02cd3d414bff9a7255554d2
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-10-12 21:34:32 -0400

    Create DMOPC '18 Contest 1 P1 - Sorting.cpp

---
+++ level-2/10-16-2021/DMOPC '18 Contest 1 P1 - Sorting.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, a;
+    cin >> n;
+    vector <int> vec1;
+    vector <int> min;
+    vector <int> max;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a;
+        vec1.emplace_back(a);
+    }
+    for (int x = 0; x < vec1.size(); x++)
+    {
+        if (vec1[x] == 0)
+        {
+            if (x == 0 || vec1[x - 1] == 0) min.emplace_back(INT_MIN);
+            else min.emplace_back(vec1[x - 1]);
+            if (x == vec1.size() - 1 || vec1[x + 1] == 0) max.emplace_back(INT_MAX);
+            else max.emplace_back(vec1[x + 1]);
+            continue;
+        }
+        if (x == vec1.size() - 1) break;
+        if (vec1[x + 1] != 0 && vec1[x] > vec1[x + 1])
+        {
+            cout << "NO" << endl;
+            return 0;
+        }
+    }
+    int max_min = INT_MIN;
+    int min_max = INT_MAX;
+    for (int a = 0; a < min.size(); a++)
+    {
+        if (min[a] > max_min) max_min = min[a];
+        if (max[a] < min_max) min_max = max[a];
+    }
+    if (min_max < max_min) cout << "NO" << endl;
+    else cout << "YES" << endl;
+}
