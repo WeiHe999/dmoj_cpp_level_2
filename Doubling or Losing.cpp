c046d21aa1edee7f6e52bdd9bfbd45f34a27dc66
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2021-12-23 15:13:42 -0500

    Create Doubling or Losing.cpp

---
+++ Doubling or Losing.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(0); cout.tie(0); cin.sync_with_stdio(0);
+    long long n;
+    cin >> n;
+    long long a = (n * 2) / 10;
+    cout << n - a << endl;
+}
