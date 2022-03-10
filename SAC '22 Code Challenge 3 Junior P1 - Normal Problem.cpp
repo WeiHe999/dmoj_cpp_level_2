eabf4e8c9462389e89d9c8d21954db3d30f4fbb0
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2022-03-09 21:41:07 -0500

    Create SAC '22 Code Challenge 3 Junior P1 - Normal Problem.cpp

---
+++ SAC '22 Code Challenge 3 Junior P1 - Normal Problem.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(0); cout.tie(0); cin.sync_with_stdio(0);
+    string str1;
+    cin >> str1;
+    if (str1.find("demello") != -1) cout << "liar" << endl;
+    else cout << "what are we going to do?" << endl;
+}
