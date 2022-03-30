b9dd07dc0ef0cb98c459b9495027f9280e04f55b
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2022-03-30 18:14:54 -0400

    Update USACO 2020 February Bronze P3 - Swapity Swap.cpp

--- USACO 2020 February Bronze P3 - Swapity Swap.cpp
+++ USACO 2020 February Bronze P3 - Swapity Swap.cpp
@@ -1,30 +1,50 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int results(int x, int a1, int a2, int b1, int b2)
+template < typename T > void print (T t)
 {
-	if (a1 <= x && x <= a2) x = a1 + a2 - x;
-	if (b1 <= x && x <= b2) x = b1 + b2 - x;
-	return x;
+    for (const auto & e:t) cout << e << " ";
+    cout << endl;
 }
- 
+
 int main()
 {
     cin.tie(0); cout.tie(0); cin.sync_with_stdio(0);
-    int n, k, a1, a2, b1, b2, k1, p, cur, res[101];
-	cin >> n >> k >> a1 >> a2 >> b1 >> b2;
-	for (int i = 1; i <= n; i++)
-	{
-		p = 1;
-		cur = results(i, a1, a2, b1, b2);
-		while (cur != i)
-		{
-			p++;
-			cur = results(cur, a1, a2, b1, b2);
-		}
-		int k1 = k % p;
-		for (int j = 0; j < k1; ++j) cur = results(cur, a1, a2, b1, b2);
-		res[cur] = i;
-	}
-	for (int i = 1; i <= n; i++) cout << res[i] << endl;
+    long long n, k, a1, a2, b1, b2;
+    cin >> n >> k >> a1 >> a2 >> b1 >> b2;
+    vector <long long> vec1 = {0};
+    unordered_map <long long, long long> m1;
+    for (long long x = 1; x <= n; x++)
+    {
+        vec1.emplace_back(x);
+    }
+    unordered_set <long long> visited;
+    vector <long long> ans(n + 1);
+    reverse(vec1.begin() + a1, vec1.begin() + a2 + 1);
+    // print(vec1);
+    reverse(vec1.begin() + b1, vec1.begin() + b2 + 1);
+    // print(vec1);
+    for (long long b = 1; b < vec1.size(); b++)
+    {
+        m1[vec1[b]] = b;
+    }
+    for (long long i = 1; i <= n; i++)
+    {
+        if (visited.count(i)) continue;
+        vector <long long> vec3;
+        for (long long cur = i; !visited.count(cur); cur = m1[cur])
+        {
+            vec3.emplace_back(cur);
+            visited.insert(cur);
+        }
+        long long rem = k % vec3.size();
+        for (long long j = 0; j < vec3.size(); j++)
+        {
+            ans[vec3[(j + rem) % vec3.size()]] = vec3[j];
+        }
+    }
+    for (long long g = 1; g < ans.size(); g++)
+    {
+        cout << ans[g] << endl;
+    }
 }
