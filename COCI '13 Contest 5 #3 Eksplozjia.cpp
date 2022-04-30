7bff74446014de582352e8f4f3026fb31eacf532
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2022-04-30 13:18:36 -0400

    Update COCI '13 Contest 5 #3 Eksplozjia.cpp

--- COCI '13 Contest 5 #3 Eksplozjia.cpp
+++ COCI '13 Contest 5 #3 Eksplozjia.cpp
@@ -1,74 +1,46 @@
-#include <deque>
-#include <string>
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-    string a, b;
-    cin >> a >> b;
+template < typename T > void print (T t)
+{
+    for (const auto & e:t) cout << e << " ";
+    cout << endl;
+}
 
-    size_t blen = b.size();
-
-    // make a = b$a
-    a = b + "$" + a;
-
-    vector<size_t> n(a.size()); // array for knuth-morris-pratt
-    vector<bool> removals(a.size()); // positions of right ends at which we remove `b`s
-
-    deque<size_t> lastN;
-    n[0] = 0;
-
-    // For the first blen + 1 iterations just do vanilla knuth-morris-pratt
-    for (size_t i = 1; i < blen + 1; ++ i) {
-        size_t z = n[i - 1];
-        while (z && a[i] != a[z]) {
-            z = n[z - 1];
-        }
-        if (a[i] != a[z]) n[i] = 0;
-        else n[i] = z + 1;
-
-        lastN.push_back(n[i]);
+string find_match(string s, string bomb, int k)
+{
+    string str1;
+    int a = k;
+    for (auto x : bomb)
+    {
+        if (s[a] != x) return str1;
+        str1 += s[a];
+        a++;
     }
+    return str1;
+}
 
-    // For the remaining iterations some characters could have been logically
-    //     removed from `a`, so use lastN to get last value of n instaed
-    //     of actually getting it from `n[i - 1]`
-    for (size_t i = blen + 1; i < a.size(); ++ i) {
-        size_t z = lastN.back();
-        while (z && a[i] != a[z]) {
-            z = n[z - 1];
-        }
-        if (a[i] != a[z]) n[i] = 0;
-        else n[i] = z + 1;
-
-        if (n[i] == blen) // found a match
+int main()
+{
+    cin.tie(0); cout.tie(0); cin.sync_with_stdio(0);
+    string s, bomb, str1="";
+    cin >> s >> bomb;
+    int b = bomb.size();
+    for (int i=0; i<s.size(); i++)
+    {
+        str1 += s[i];
+        if (str1.size()>=b)
         {
-            removals[i] = true;
-
-            // kill last |b| - 1 `n_i`s
-            for (size_t j = 0; j < blen - 1; ++ j) {
-                lastN.pop_back();
+            if (str1.substr(str1.size()-b)==bomb)
+            {
+                //explode
+                for (int j=0; j<b; j++) str1.pop_back();
+                //cout << "after bomb, str1=" << str1 << endl;
             }
         }
-        else {
-            lastN.push_back(n[i]);
-        }
     }
 
-    string ret;
-    size_t toRemove = 0;
-    for (size_t pos = a.size() - 1; a[pos] != '$'; -- pos) {
-        if (removals[pos]) toRemove += blen;
-        if (toRemove) -- toRemove;
-        else ret.push_back(a[pos]);
-    }
-    reverse(ret.begin(), ret.end());
-
-    if (ret.size()>0) cout << ret << endl;
+  
+    if (str1.size() != 0) cout << str1 << endl;
     else cout << "FRULA" << endl;
-
-    return 0;
 }
