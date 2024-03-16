b7bab3e6a7d2b5b8c221f71ceb2af510ac3d1edb
WeiHe999 <86860078+WeiHe999@users.noreply.github.com>
2024-03-16 13:37:11 -0400

    Update King Graff's Defense.cpp

--- King Graff's Defense.cpp
+++ King Graff's Defense.cpp
@@ -1,78 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
+//#define int long long
 const int MM = 1e6+1;
 
-unordered_map <int, vector <int> > graph;
-unordered_map <int, int> comp_size;
-int dfn[MM], low[MM];
-set<pair<int, int> > bridges;
-int ind = 0;
-int comp_id = 0;
+int dfn[MM], low[MM], head[MM], v[2*MM], nxt[2*MM];
+
+
+stack <int> s1;
+int i = 0;
+long long f = 0;
+int e_id = 0;
 
 void tarjan(int cur, int prev)
 {
-    ind++;
-    dfn[cur] = low[cur] = ind;
-    for (auto x : graph[cur])
+    i++;
+    dfn[cur] = low[cur] = i;
+    s1.push(cur);
+   for (int i = head[cur]; i!=-1; i=nxt[i])
     {
+        int x = v[i];
         if (x == prev) continue;
         if (dfn[x] == 0)
         {
             tarjan(x, cur);
-            low[cur] = min(low[x], low[cur]);
-            if (low[cur] < low[x]) bridges.insert({min(cur, x), max(cur, x)});
+            low[cur] = min(low[cur], low[x]);
         }
-        else
-        {
-            low[cur] = min(dfn[x], low[cur]);
-        }
+        else low[cur] = min(low[cur], dfn[x]);
     }
-}
-
-void dfs(int cur)
-{
-    low[cur] = 1;
-    comp_size[comp_id]++;
-    for (auto x: graph[cur])
+    if (dfn[cur] == low[cur])
     {
-        if (!low[x] && !bridges.count({min(cur, x), max(cur, x)}))
+        vector <int> vec1;
+        while (s1.top() != cur)
         {
-            dfs(x);
+            vec1.emplace_back(s1.top());
+            s1.pop();
         }
+        vec1.emplace_back(s1.top());
+        long long ss = vec1.size();
+        f += (ss * (ss -1ll))/2ll;
+        
+        s1.pop();
     }
 }
 
-int main()
-{
-    cin.tie(0); cout.tie(0); cin.sync_with_stdio(0);
-    memset(dfn, 0, sizeof(dfn));
-    memset(low, 0, sizeof(low));
-    long long n, m, a, b, s = 0;
+
+
+
+int32_t main() {
+   
+    int n, m, a, b;
     cin >> n >> m;
-    for (long long i = 1; i <= m; i++)
+    memset(head, -1, sizeof(head));
+   for (int i = 1; i <= m; i++)
     {
         cin >> a >> b;
-        graph[a].emplace_back(b);
-        graph[b].emplace_back(a);
+        v[e_id] = b;
+        nxt[e_id] = head[a];
+        head[a] = e_id;
+        e_id++;
+        
+        v[e_id] = a;
+        nxt[e_id] = head[b];
+        head[b] = e_id;
+        e_id++;
+
     }
     tarjan(1, -1);
-    // for (auto x:bridges)
-    // {
-    //     cout << x.first << "-->" << x.second << endl;
-    // }
     
-    memset(low, 0, sizeof(low));
-    for (long long i = 1; i <= n; i++)
-    {
-        if (low[i]==0)
-        {
-            comp_id++;
-            dfs(i);
-        }
-    }
-
-    for (auto x : comp_size) s += ((x.second * (x.second - 1l)) / 2l);
-    long long denominator = (n * (n - 1l)) / 2l;
-    double numerator = (denominator - s) * 1.0;
-    cout << fixed << setprecision(5) << numerator / denominator << "\n";
+    
+    long long m1 = 1ll*(long long) n * ((long long) n - 1ll);
+    cout << fixed << setprecision(5) << (1.0 - 1.0 * f / (m1 / 2ll));    
 }
